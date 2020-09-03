f3c26fdad3290d77d62025bf19969ba4ea442dc7
Wendell Edgard <wendellcyg@gmail.com>
2020-09-03 18:54:24 -0300

    feature/AG-01 layout com tailwindcss

---
+++ resources/js/components/CompanyComponent.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+<p>My first component
+</p>
+</div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                companies: []
+            }
+        },
+        mounted() {
+
+        },
+        methods: {
+            loadCompanies: function () {
+            }
+        }
+
+    }
+</script>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container mx-auto px-4 pt-16">
+        <div class="secao-comprar">
+            <h2 class="uppercase text-green-500 text-lg font-semibold tracking-wider">
+                COMPRAR
+            </h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="{{ asset('/storage/images/agro.jpg') }}" class="hover:opacity-75 transition ease-in-out duration-500">
+                    </a>
+                    <div class="mt-3">
+                        <a href="#" class="text-lg text-gray-500 hover:text-gray-300">Produto X</a>
+                        <div class="flex items-center mt-1 text-sm">
+                            Empresa | Natal/RN
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="{{ asset('/storage/images/agro.jpg') }}" class="hover:opacity-75 transition ease-in-out duration-500">
+                    </a>
+                    <div class="mt-3">
+                        <a href="#" class="text-lg text-gray-500 hover:text-gray-300">Produto X</a>
+                        <div class="flex items-center mt-1 text-sm">
+                            Empresa | Natal/RN
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="{{ asset('/storage/images/agro.jpg') }}" class="hover:opacity-75 transition ease-in-out duration-500">
+                    </a>
+                    <div class="mt-3">
+                        <a href="#" class="text-lg text-gray-500 hover:text-gray-300">Produto X</a>
+                        <div class="flex items-center mt-1 text-sm">
+                            Empresa | Natal/RN
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="{{ asset('/storage/images/agro.jpg') }}" class="hover:opacity-75 transition ease-in-out duration-500">
+                    </a>
+                    <div class="mt-3">
+                        <a href="#" class="text-lg text-gray-500 hover:text-gray-300">Produto X</a>
+                        <div class="flex items-center mt-1 text-sm">
+                            Empresa | Natal/RN
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="{{ asset('/storage/images/agro.jpg') }}" class="hover:opacity-75 transition ease-in-out duration-500">
+                    </a>
+                    <div class="mt-3">
+                        <a href="#" class="text-lg text-gray-500 hover:text-gray-300">Produto X</a>
+                        <div class="flex items-center mt-1 text-sm">
+                            Empresa | Natal/RN
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="{{ asset('/storage/images/agro.jpg') }}" class="hover:opacity-75 transition ease-in-out duration-500">
+                    </a>
+                    <div class="mt-3">
+                        <a href="#" class="text-lg text-gray-500 hover:text-gray-300">Produto X</a>
+                        <div class="flex items-center mt-1 text-sm">
+                            Empresa | Natal/RN
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="{{ asset('/storage/images/agro.jpg') }}" class="hover:opacity-75 transition ease-in-out duration-500">
+                    </a>
+                    <div class="mt-3">
+                        <a href="#" class="text-lg text-gray-500 hover:text-gray-300">Produto X</a>
+                        <div class="flex items-center mt-1 text-sm">
+                            Empresa | Natal/RN
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="{{ asset('/storage/images/agro.jpg') }}" class="hover:opacity-75 transition ease-in-out duration-500">
+                    </a>
+                    <div class="mt-3">
+                        <a href="#" class="text-lg text-gray-500 hover:text-gray-300">Produto X</a>
+                        <div class="flex items-center mt-1 text-sm">
+                            Empresa | Natal/RN
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
